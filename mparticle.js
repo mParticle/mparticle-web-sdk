47524d428f12e701d4a20bc3712d00b4ff5914a4
Tiarnan Breffni <tbreffni@gmail.com>
2016-07-01 13:29:56 -0400

    Call setCookie() after parsing response from server

--- mparticle.js
+++ mparticle.js
@@ -20,7 +20,7 @@
     var serviceUrl = "jssdk.mparticle.com/v1/JS/",
         secureServiceUrl = "jssdks.mparticle.com/v1/JS/",
         serviceScheme = window.location.protocol + '//',
-        sdkVersion = '1.8.1',
+        sdkVersion = '1.8.2',
         isEnabled = true,
         pluses = /\+/g,
         sessionAttributes = {},
@@ -721,6 +721,8 @@
                         serverSettings[prop] = fullProp;
                     }
                 }
+
+                setCookie();
             }
         }
         catch (e) {
