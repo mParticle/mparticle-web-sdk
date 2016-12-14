df752ece567913510ff331c73665fe547b68fcaa
hackerrdave <davekerrcode@gmail.com>
2016-12-13 21:51:15 -0500

    add test npm command w/ browser-sync

---
+++ test/runner.js
@@ -0,0 +1,13 @@
+const browserSync = require('browser-sync').create();
+
+browserSync.watch('mparticle.js').on('change', browserSync.reload);
+browserSync.watch('test/*.js').on('change', browserSync.reload);
+browserSync.watch('test/index.html').on('change', browserSync.reload);
+
+browserSync.init({
+  port: 3000,
+  startPath: '/test/index.html',
+  server: {
+    index: 'test/index.html'
+  }
+});
