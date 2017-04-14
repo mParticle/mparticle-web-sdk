511ee60213fb7ad398ede5697fc886582ac00142
Robert Ing <ring@mparticle.com>
2017-04-14 13:08:13 -0400

    Update version to 1.9.0

--- mparticle.js
+++ mparticle.js
@@ -20,7 +20,7 @@
     var serviceUrl = 'jssdk.mparticle.com/v1/JS/',
         secureServiceUrl = 'jssdks.mparticle.com/v1/JS/',
         serviceScheme = window.location.protocol + '//',
-        sdkVersion = '1.8.7',
+        sdkVersion = '1.9.0',
         isEnabled = true,
         pluses = /\+/g,
         sessionAttributes = {},
