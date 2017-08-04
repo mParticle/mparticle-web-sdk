a5ddf88bba3dfe6793e90701a095095273d4e186
Robert Ing <ring@mparticle.com>
2017-08-04 10:10:52 -0400

    Update version to 2.0.5

--- mparticle.js
+++ mparticle.js
@@ -21,7 +21,7 @@
         secureServiceUrl = 'jssdks.mparticle.com/v2/JS/',
         identityUrl = 'https://identity.mparticle.com/v1/', //prod
         serviceScheme = window.location.protocol + '//',
-        sdkVersion = '2.0.4',
+        sdkVersion = '2.0.5',
         sdkVendor = 'mparticle',
         platform = 'web',
         isEnabled = true,
