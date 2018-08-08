5ea9507662f8c51160c57d6b6ff017406ee844d3
mParticle Automation <developers@mparticle.com>
2018-08-08 10:34:59 -0400

    Generate latest bundle

--- mparticle.js
+++ mparticle.js
@@ -1,4 +1,4 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 var Helpers = require('./helpers'),
     Constants = require('./constants'),
     HTTPCodes = Constants.HTTPCodes,
@@ -333,7 +333,7 @@ var v1ServiceUrl = 'jssdk.mparticle.com/v1/JS/',
     v2ServiceUrl = 'jssdk.mparticle.com/v2/JS/',
     v2SecureServiceUrl = 'jssdks.mparticle.com/v2/JS/',
     identityUrl = 'https://identity.mparticle.com/v1/', //prod
-    sdkVersion = '2.6.4',
+    sdkVersion = '2.7.0',
     sdkVendor = 'mparticle',
     platform = 'web',
     Messages = {
@@ -2775,6 +2775,25 @@ function mParticleUser(mpid) {
 
                     Forwarders.callSetUserAttributeOnForwarders(key, value);
                 }
+            }
+        },
+        /**
+        * Set multiple user attributes
+        * @method setUserAttributes
+        * @param {Object} user attribute object with keys of the attribute type, and value of the attribute value
+        */
+        setUserAttributes: function(userAttributes) {
+            mParticle.sessionManager.resetSessionTimer();
+            if (Helpers.isObject(userAttributes)) {
+                if (Helpers.canLog()) {
+                    for (var key in userAttributes) {
+                        if (userAttributes.hasOwnProperty(key)) {
+                            this.setUserAttribute(key, userAttributes[key]);
+                        }
+                    }
+                }
+            } else {
+                Helpers.debug('Must pass an object into setUserAttributes. You passed a ' + typeof userAttributes);
             }
         },
         /**
