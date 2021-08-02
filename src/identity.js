a864aefc2db11d63d57174e92b6c5965a93471a8
Luã de Souza <eumesmo.ht@gmail.com>
2021-08-02 23:58:05 +1000

    feat: Allow scope param on user alias requests
    In some scenarios, it might be necessary to define the alias scope to
    something other than the default "device" option. This is not allowed on
    the web SDK at the moment, even though it is on the Identity API.

--- src/identity.js
+++ src/identity.js
@@ -192,6 +192,7 @@ export default function Identity(mpInstance) {
                     source_mpid: aliasRequest.sourceMpid,
                     start_unixtime_ms: aliasRequest.startTime,
                     end_unixtime_ms: aliasRequest.endTime,
+                    scope: aliasRequest.scope,
                     device_application_stamp: mpInstance._Store.deviceId,
                 },
             };
