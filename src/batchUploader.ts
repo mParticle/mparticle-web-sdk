eddbc28b8a2a8daefb7cdcbc5dde17043bf30bde
Alexander Sapountzis <alexs@mparticle.com>
2022-12-28 15:09:36 -0500

    Incremental introduction of batch persistence for testing

--- src/batchUploader.ts
+++ src/batchUploader.ts
@@ -257,8 +257,8 @@ export class BatchUploader {
         // that is already in transit
         if (remainingUploads && remainingUploads.length) {
             this.pendingUploads.unshift(...remainingUploads);
-            // } else if (!isEmpty(this.batchVault.contents)) {
-            //     this.pendingUploads.push(...this.batchVault.retrieveItems());
+        } else if (!isEmpty(this.batchVault.contents)) {
+            this.pendingUploads.push(...this.batchVault.retrieveItems());
         }
 
         if (triggerFuture) {
