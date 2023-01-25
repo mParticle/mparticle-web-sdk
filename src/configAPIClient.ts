94e18873ae79712f9c52345e25279ef390613d08
Alex S <49695018+alexs-mparticle@users.noreply.github.com>
2023-01-25 10:01:45 -0500

    refactor: Migrate Config API Client to TypeScript (#382)

---
+++ src/configAPIClient.ts
@@ -0,0 +1,141 @@
+import { DataPlanConfig } from '@mparticle/web-sdk';
+import {
+    BooleanStringLowerCase,
+    DataPlanResult,
+    MParticleWebSDK,
+    SDKEventCustomFlags,
+    SDKInitConfig,
+} from './sdkRuntimeModels';
+import { Dictionary } from './utils';
+
+export type SDKCompleteInitCallback = (
+    apiKey: string,
+    config: SDKInitConfig,
+    mpInstance: MParticleWebSDK
+) => void;
+
+export interface IKitConfigs {
+    name: string;
+    moduleId: number;
+    isDebug: boolean;
+    isVisible: boolean;
+    isDebugString: BooleanStringLowerCase;
+    hasDebugString: BooleanStringLowerCase;
+    settings: Dictionary;
+    screenNameFilters: number[];
+    screenAttributeFilters: number[];
+    userIdentityFilters: number[];
+    userAttributeFilters: number[];
+    eventNameFilters: number[];
+    eventTypeFilters: number[];
+    attributeFilters: number[];
+    filteringEventAttributeValue: Dictionary;
+    filteringConsentRuleValues: Dictionary;
+    consentRegulationFilters: number[];
+    consentRegulationPurposeFilters: number[];
+    messageTypeFilters: number[];
+    messageTypeStateFilters: number[];
+    eventSubscriptionId: number;
+    excludeAnonymousUser: boolean;
+}
+
+export interface IPixelConfig {
+    name: string;
+    moduleId: number;
+    esId: number;
+    isDebug: boolean;
+    isProduction: boolean;
+    settings: Dictionary;
+    frequencyCap: number;
+    pixelUrl: string;
+    redirectUrl: string;
+}
+
+export interface IConfigResponse {
+    appName: string;
+    dataPlanResult: DataPlanResult;
+    kitConfigs: IKitConfigs[];
+    serviceUrl: string;
+    secureServiceUrl: string;
+    minWebviewBridgeVersion: number;
+    workspaceToken: string;
+    pixelConfigs: IPixelConfig[];
+    flags: SDKEventCustomFlags;
+}
+
+export interface IConfigAPIClient {
+    getSDKConfiguration: (
+        apiKey: string,
+        config: SDKInitConfig,
+        completeSDKInitialization: SDKCompleteInitCallback,
+        mpInstance: MParticleWebSDK
+    ) => void;
+}
+
+export default function ConfigAPIClient(this: IConfigAPIClient) {
+    this.getSDKConfiguration = (
+        apiKey,
+        config,
+        completeSDKInitialization,
+        mpInstance
+    ): void => {
+        let url: string;
+        try {
+            const xhrCallback = function() {
+                if (xhr.readyState === 4) {
+                    // when a 200 returns, merge current config with what comes back from config, prioritizing user inputted config
+                    if (xhr.status === 200) {
+                        config = mpInstance._Helpers.extend(
+                            {},
+                            config,
+                            JSON.parse(xhr.responseText)
+                        );
+                        completeSDKInitialization(apiKey, config, mpInstance);
+                        mpInstance.Logger.verbose(
+                            'Successfully received configuration from server'
+                        );
+                    } else {
+                        // if for some reason a 200 doesn't return, then we initialize with the just the passed through config
+                        completeSDKInitialization(apiKey, config, mpInstance);
+                        mpInstance.Logger.verbose(
+                            'Issue with receiving configuration from server, received HTTP Code of ' +
+                                xhr.status
+                        );
+                    }
+                }
+            };
+
+            const xhr = mpInstance._Helpers.createXHR(xhrCallback);
+            url =
+                'https://' +
+                mpInstance._Store.SDKConfig.configUrl +
+                apiKey +
+                '/config?env=';
+            if (config.isDevelopmentMode) {
+                url = url + '1';
+            } else {
+                url = url + '0';
+            }
+
+            const dataPlan = config.dataPlan as DataPlanConfig;
+            if (dataPlan) {
+                if (dataPlan.planId) {
+                    url = url + '&plan_id=' + dataPlan.planId || '';
+                }
+                if (dataPlan.planVersion) {
+                    url = url + '&plan_version=' + dataPlan.planVersion || '';
+                }
+            }
+
+            if (xhr) {
+                xhr.open('get', url);
+                xhr.send(null);
+            }
+        } catch (e) {
+            completeSDKInitialization(apiKey, config, mpInstance);
+            mpInstance.Logger.error(
+                'Error getting forwarder configuration from mParticle servers.'
+            );
+        }
+    };
+}
---
+++ test/src/tests-config-api-client.ts
@@ -0,0 +1,214 @@
+import sinon from 'sinon';
+import { urls } from './config';
+import { apiKey, MPConfig } from './config';
+import { expect } from 'chai';
+import ConfigAPIClient, {
+    IConfigAPIClient,
+    IConfigResponse,
+} from '../../src/configAPIClient';
+import {
+    DataPlanConfig,
+    DataPlanResult,
+    MParticleWebSDK,
+    SDKInitConfig,
+} from '../../src/sdkRuntimeModels';
+
+declare global {
+    interface Window {
+        mParticle: MParticleWebSDK;
+    }
+}
+
+describe('ConfigAPIClient', () => {
+    let mockServer;
+    let sdkInitCompleteCallback;
+    let configUrl;
+    let dataPlan: DataPlanConfig;
+    let dataPlanResult: DataPlanResult;
+
+    beforeEach(() => {
+        mockServer = sinon.createFakeServer();
+        mockServer.respondImmediately = true;
+        sdkInitCompleteCallback = sinon.spy();
+        window.mParticle._resetForTests(MPConfig);
+    });
+
+    afterEach(() => {
+        mockServer.reset();
+        sinon.restore();
+    });
+
+    describe('#getSDKConfiguration', () => {
+        describe('with defaults', () => {
+            it('should fetch a config from the server', () => {
+                const config = { requestConfig: true } as SDKInitConfig;
+
+                mockServer.respondWith(urls.config, [
+                    200,
+                    {},
+                    JSON.stringify({ appName: 'Test App', kitConfigs: [] }),
+                ]);
+
+                window.mParticle.init(apiKey, config);
+
+                const configAPIClient = new ConfigAPIClient();
+                configAPIClient.getSDKConfiguration(
+                    apiKey,
+                    config,
+                    sdkInitCompleteCallback,
+                    window.mParticle.getInstance()
+                );
+
+                expect(sdkInitCompleteCallback.getCalls().length).to.equal(1);
+                expect(
+                    sdkInitCompleteCallback.calledWithMatch(
+                        apiKey,
+                        MPConfig,
+                        window.mParticle.getInstance()
+                    )
+                );
+                expect(mockServer.lastRequest).to.haveOwnProperty('response');
+
+                // TODO: Check mockServer
+                expect(mockServer.lastRequest.url).to.equal(urls.config);
+
+                const configResponse: IConfigResponse = JSON.parse(
+                    mockServer.lastRequest.response
+                );
+
+                expect(configResponse).to.be.ok;
+                expect(configResponse.appName).to.equal('Test App');
+                expect(configResponse.kitConfigs).to.be.ok;
+            });
+
+            it('should continue sdk init if fetch fails', () => {
+                const config = { requestConfig: true };
+
+                mockServer.respondWith(urls.config, [400, {}, '']);
+
+                window.mParticle.init(apiKey, window.mParticle.config);
+
+                const configAPIClient = new ConfigAPIClient();
+                configAPIClient.getSDKConfiguration(
+                    apiKey,
+                    config,
+                    sdkInitCompleteCallback,
+                    window.mParticle.getInstance()
+                );
+
+                expect(sdkInitCompleteCallback.getCalls().length).to.equal(1);
+                expect(
+                    sdkInitCompleteCallback.calledWithMatch(
+                        apiKey,
+                        MPConfig,
+                        window.mParticle.getInstance()
+                    )
+                );
+                expect(mockServer.lastRequest).to.haveOwnProperty('response');
+                expect(mockServer.lastRequest.url).to.equal(urls.config);
+            });
+        });
+
+        describe('with Data Plan', () => {
+            beforeEach(() => {
+                configUrl = `${urls.config}&plan_id=test_data_plan&plan_version=42`;
+
+                dataPlan = {
+                    planId: 'test_data_plan',
+                    planVersion: 42,
+                };
+
+                dataPlanResult = {
+                    dtpn: {
+                        blok: {
+                            ev: false,
+                            ea: false,
+                            ua: false,
+                            id: false,
+                        },
+                        vers: {
+                            version: 17,
+                            data_plan_id: 'fake_data_plan',
+                            last_modified_on: '2022-02-16T17:52:19.13Z',
+                            version_document: {
+                                data_points: [],
+                            },
+                        },
+                    },
+                };
+
+                mockServer.respondWith(configUrl, [
+                    200,
+                    {},
+                    JSON.stringify({
+                        appName: 'Test App',
+                        kitConfigs: [],
+                        dataPlanResult,
+                    }),
+                ]);
+            });
+
+            it('appends data plan information to url', () => {
+                const config: Partial<SDKInitConfig> = {
+                    requestConfig: true,
+                    dataPlan,
+                };
+
+                window.mParticle.init(apiKey, window.mParticle.config);
+
+                const configAPIClient = new ConfigAPIClient();
+                configAPIClient.getSDKConfiguration(
+                    apiKey,
+                    config,
+                    sdkInitCompleteCallback,
+                    window.mParticle.getInstance()
+                );
+
+                expect(sdkInitCompleteCallback.getCalls().length).to.equal(1);
+                expect(
+                    sdkInitCompleteCallback.calledWithMatch(
+                        apiKey,
+                        MPConfig,
+                        window.mParticle.getInstance()
+                    )
+                );
+
+                expect(mockServer.lastRequest).to.haveOwnProperty('url');
+                expect(mockServer.lastRequest.url).to.equal(configUrl);
+            });
+
+            it('returns data plan information within the HTTP Response', () => {
+                const configUrl = `${urls.config}&plan_id=test_data_plan&plan_version=42`;
+                const config = ({
+                    requestConfig: true,
+                    dataPlan: dataPlan,
+                } as unknown) as SDKInitConfig;
+
+                window.mParticle.init(apiKey, window.mParticle.config);
+
+                const configAPIClient: IConfigAPIClient = new ConfigAPIClient();
+                configAPIClient.getSDKConfiguration(
+                    apiKey,
+                    config,
+                    sdkInitCompleteCallback,
+                    window.mParticle.getInstance()
+                );
+
+                expect(mockServer.lastRequest).to.haveOwnProperty('method');
+                expect(mockServer.lastRequest).to.haveOwnProperty('response');
+
+                expect(mockServer.lastRequest.method).to.equal('get');
+                expect(mockServer.lastRequest.url).to.equal(configUrl);
+
+                const configResponse: IConfigResponse = JSON.parse(
+                    mockServer.lastRequest.response
+                );
+
+                expect(configResponse).to.be.ok;
+                expect(configResponse.appName).to.equal('Test App');
+                expect(configResponse.kitConfigs).to.be.ok;
+                expect(configResponse.dataPlanResult).to.be.ok;
+            });
+        });
+    });
+});
