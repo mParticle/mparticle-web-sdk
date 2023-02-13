34d7569fd181ac0f98dd407c13ec819142393945
Alexander Sapountzis <alexs@mparticle.com>
2023-02-13 13:37:39 -0500

    docs: Add Comments for Cookie Sync Module

--- src/cookieSyncManager.js
+++ src/cookieSyncManager.js
@@ -4,7 +4,10 @@ var Messages = Constants.Messages;
 
 export default function cookieSyncManager(mpInstance) {
     var self = this;
+
+    // Public
     this.attemptCookieSync = function(previousMPID, mpid, mpidIsNotInCookies) {
+        // TODO: These should move inside the for loop
         var pixelConfig,
             lastSyncDateForModule,
             url,
@@ -12,6 +15,7 @@ export default function cookieSyncManager(mpInstance) {
             urlWithRedirect,
             requiresConsent;
 
+        // TODO: Make this exit quicker instead of nested
         if (mpid && !mpInstance._Store.webviewBridgeEnabled) {
             mpInstance._Store.pixelConfigurations.forEach(function(
                 pixelSettings
@@ -20,6 +24,7 @@ export default function cookieSyncManager(mpInstance) {
                 // set to true only if filteringConsenRuleValues.values.length exists
                 requiresConsent = false;
 
+                // TODO: Replace with isEmpty
                 if (
                     pixelSettings.filteringConsentRuleValues &&
                     pixelSettings.filteringConsentRuleValues.values &&
@@ -27,24 +32,40 @@ export default function cookieSyncManager(mpInstance) {
                 ) {
                     requiresConsent = true;
                 }
+
                 pixelConfig = {
+                    // Kit Module ID
                     moduleId: pixelSettings.moduleId,
+
+                    // Tells you how often we should do a cookie sync (in days)
                     frequencyCap: pixelSettings.frequencyCap,
+
+                    // Url for cookie sync pixel
                     pixelUrl: self.replaceAmp(pixelSettings.pixelUrl),
+
+                    // TODO: Document requirements for redirectUrl
                     redirectUrl: pixelSettings.redirectUrl
                         ? self.replaceAmp(pixelSettings.redirectUrl)
                         : null,
+
+                    // Filtering rules as defined in UI
                     filteringConsentRuleValues:
                         pixelSettings.filteringConsentRuleValues,
                 };
 
+                // TODO: combine replaceMPID and replaceAMp into sanitizeUrl function
                 url = self.replaceMPID(pixelConfig.pixelUrl, mpid);
                 redirect = pixelConfig.redirectUrl
                     ? self.replaceMPID(pixelConfig.redirectUrl, mpid)
                     : '';
                 urlWithRedirect = url + encodeURIComponent(redirect);
+
+                // TODO: Do we need to fetch persistence during every loop,
+                //       or can we fetch once and cache?
                 var persistence = mpInstance._Persistence.getPersistence();
 
+                // TODO: Is there a historic reason for checking for previousMPID?
+                //       it does not appear to be passed in anywhere
                 if (previousMPID && previousMPID !== mpid) {
                     if (persistence && persistence[mpid]) {
                         if (!persistence[mpid].csd) {
@@ -62,6 +83,8 @@ export default function cookieSyncManager(mpInstance) {
                     }
                     return;
                 } else {
+                    // TODO: can we check for the inverse and exit early
+                    //       rather than nesting?
                     if (persistence[mpid]) {
                         if (!persistence[mpid].csd) {
                             persistence[mpid].csd = {};
@@ -77,6 +100,7 @@ export default function cookieSyncManager(mpInstance) {
                         if (lastSyncDateForModule) {
                             // Check to see if we need to refresh cookieSync
                             if (
+                                // Can this be turned into a convenience method for readability?
                                 new Date().getTime() >
                                 new Date(lastSyncDateForModule).getTime() +
                                     pixelConfig.frequencyCap *
@@ -112,14 +136,18 @@ export default function cookieSyncManager(mpInstance) {
         }
     };
 
+    // Private
     this.replaceMPID = function(string, mpid) {
         return string.replace('%%mpid%%', mpid);
     };
 
+    // Private
+    // TODO: Rename function to replaceAmpWithAmpersand
     this.replaceAmp = function(string) {
         return string.replace(/&amp;/g, '&');
     };
 
+    // Private
     this.performCookieSync = function(
         url,
         moduleId,
@@ -135,6 +163,11 @@ export default function cookieSyncManager(mpInstance) {
         if (requiresConsent && mpidIsNotInCookies) {
             return;
         }
+
+        // TODO: attemptCookieSync is called as a loop and therefore this function
+        //       polls the user object and consent multiple times.
+        //       Could we factor this out or cache the boolean so that it
+        //       only gets called once per cookie sync attempt per module?
         if (
             mpInstance._Consent.isEnabledForUserConsent(
                 filteringConsentRuleValues,
@@ -145,6 +178,7 @@ export default function cookieSyncManager(mpInstance) {
 
             mpInstance.Logger.verbose(Messages.InformationMessages.CookieSync);
             img.onload = function() {
+                // TODO: Break this out into a convenience method so we can unit test
                 cookieSyncDates[moduleId.toString()] = new Date().getTime();
                 mpInstance._Persistence.saveUserCookieSyncDatesToPersistence(
                     mpid,
