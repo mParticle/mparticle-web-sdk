2af3494b7e6821699b7982fb9a9a53153d320cae
Alex S <49695018+alexs-mparticle@users.noreply.github.com>
2023-03-06 13:50:54 -0500

    refactor: Simplify Vault for Offline Storage (#396)

--- src/vault.ts
+++ src/vault.ts
@@ -6,12 +6,10 @@ interface IVaultOptions {
     offlineStorageEnabled?: boolean;
 }
 
-export default class Vault<StorableItem extends Dictionary> {
-    public contents: Dictionary<StorableItem>;
+export default class Vault<StorableItem> {
+    public contents: StorableItem;
     private readonly _storageKey: string;
-    private readonly _itemKey: keyof StorableItem;
     private logger?: Logger;
-    private offlineStorageEnabled: boolean = false;
 
     /**
      *
@@ -19,16 +17,9 @@ export default class Vault<StorableItem extends Dictionary> {
      * @param {string} itemKey an element within your StorableItem to use as a key
      * @param {IVaultOptions} options A Dictionary of IVaultOptions
      */
-    constructor(
-        storageKey: string,
-        itemKey: keyof StorableItem,
-        options?: IVaultOptions
-    ) {
+    constructor(storageKey: string, options?: IVaultOptions) {
         this._storageKey = storageKey;
-        this._itemKey = itemKey;
-        this.contents = this.getItems() || {};
-
-        this.offlineStorageEnabled = options?.offlineStorageEnabled;
+        this.contents = this.getFromLocalStorage();
 
         // Add a fake logger in case one is not provided or needed
         this.logger = options?.logger || {
@@ -39,51 +30,16 @@ export default class Vault<StorableItem extends Dictionary> {
     }
 
     /**
-     * Stores a single Item using `itemId` as an index
+     * Stores a StorableItem to Local Storage
      * @method storeItem
-     * @param item {StorableItem} a Dictonary with key to store
+     * @param item {StorableItem}
      */
-    public storeItem(item: StorableItem): void {
-        this.contents = this.getItems();
+    public store(item: StorableItem): void {
+        this.contents = item;
 
-        if (item[this._itemKey]) {
-            this.contents[item[this._itemKey]] = item;
-            this.logger.verbose(
-                `Saved items to vault with key: ${item[this._itemKey]}`
-            );
-        }
+        this.logger.verbose(`Saved to local storage: ${item}`);
 
-        this.saveItems(this.contents);
-    }
-
-    /**
-     * Stores Items using `itemId` as an index
-     * @method storeItems
-     * @param {StorableItem[]} an Array of StorableItems
-     */
-    public storeItems(items: StorableItem[]): void {
-        items.forEach((item) => this.storeItem(item));
-    }
-
-    /**
-     * Removes a single StorableItem based on `indexId`
-     * @method removeItem
-     * @param {String} indexId
-     */
-    public removeItem(indexId: string): void {
-        this.logger.verbose(`Removing from vault: ${indexId}`);
-        this.contents = this.getItems() || {};
-
-        try {
-            delete this.contents[indexId];
-
-            this.saveItems(this.contents);
-        } catch (error) {
-            this.logger.error(
-                `Unable to remove item without a matching ID ${indexId}`
-            );
-            this.logger.error(error as string);
-        }
+        this.saveToLocalStorage(this.contents);
     }
 
     /**
@@ -91,10 +47,10 @@ export default class Vault<StorableItem extends Dictionary> {
      * @method retrieveItems
      * @returns {StorableItem[]} an array of Items
      */
-    public retrieveItems(): StorableItem[] {
-        this.contents = this.getItems();
+    public retrieve(): StorableItem {
+        this.contents = this.getFromLocalStorage();
 
-        return Object.keys(this.contents).map((item) => this.contents[item]);
+        return this.contents;
     }
 
     /**
@@ -102,14 +58,11 @@ export default class Vault<StorableItem extends Dictionary> {
      * @method purge
      */
     public purge(): void {
-        this.contents = {};
-        this.removeItems();
+        this.contents = null;
+        this.removeFromLocalStorage();
     }
 
-    private saveItems(items: Dictionary<StorableItem>): void {
-        if (!this.offlineStorageEnabled) {
-            return;
-        }
+    private saveToLocalStorage(items: StorableItem): void {
         try {
             window.localStorage.setItem(
                 this._storageKey,
@@ -121,21 +74,15 @@ export default class Vault<StorableItem extends Dictionary> {
         }
     }
 
-    private getItems(): Dictionary<StorableItem> {
-        if (!this.offlineStorageEnabled) {
-            return this.contents;
-        }
+    private getFromLocalStorage(): StorableItem | null {
         // TODO: Handle cases where Local Storage is unavailable
         // https://go.mparticle.com/work/SQDSDKS-5022
-        const itemString = window.localStorage.getItem(this._storageKey);
+        const item: string = window.localStorage.getItem(this._storageKey);
 
-        return itemString ? JSON.parse(itemString) : {};
+        return item ? JSON.parse(item) : null;
     }
 
-    private removeItems(): void {
-        if (!this.offlineStorageEnabled) {
-            return;
-        }
+    private removeFromLocalStorage(): void {
         window.localStorage.removeItem(this._storageKey);
     }
 }
