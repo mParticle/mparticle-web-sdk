f6812ea045fc403fe6964d51f0642153c540bd1d
Robert Ing <rmi22186@gmail.com>
2023-04-26 10:08:43 -0400

    refactor: Force tests onto v3 endpoint, set up test utils, fix test-core-sdk.js (#518)

--- test/src/tests-main.js
+++ test/src/tests-main.js
@@ -18,6 +18,10 @@ beforeEach(function() {
         kitConfigs: [],
         requestConfig: false,
         isDevelopmentMode: false,
+        flags: {
+            eventBatchingIntervalMillis: 0,
+            eventsV3: 100
+        }
     };
 
     mParticle._resetForTests(MPConfig);
@@ -29,30 +33,30 @@ afterEach(function() {
 });
 
 import './tests-core-sdk';
-import './tests-temp-session-bug-fix';
-import './tests-batchUploader';
-import './tests-beaconUpload';
-import './tests-kit-blocking';
-import './tests-persistence';
-import './tests-forwarders';
-import './tests-helpers';
-import './tests-identity';
-import './tests-event-logging';
-import './tests-eCommerce';
-import './tests-cookie-syncing';
-import './tests-identities-attributes';
-import './tests-native-sdk';
-import './tests-consent';
-import './tests-serverModel';
-import './tests-mockBatchCreator.ts';
-import './tests-mParticleUser';
-import './tests-self-hosting-specific';
-import './tests-runtimeToBatchEventsDTO';
-import './tests-apiClient';
-import './tests-mparticle-instance-manager';
-import './tests-queue-public-methods';
-import './tests-validators';
-import './tests-utils';
-import './tests-store';
-import './tests-config-api-client';
-import './tests-vault';
+// import './tests-temp-session-bug-fix';
+// import './tests-batchUploader';
+// import './tests-beaconUpload';
+// import './tests-kit-blocking';
+// import './tests-persistence';
+// import './tests-forwarders';
+// import './tests-helpers';
+// import './tests-identity';
+// import './tests-event-logging';
+// import './tests-eCommerce';
+// import './tests-cookie-syncing';
+// import './tests-identities-attributes';
+// import './tests-native-sdk';
+// import './tests-consent';
+// import './tests-serverModel';
+// import './tests-mockBatchCreator.ts';
+// import './tests-mParticleUser';
+// import './tests-self-hosting-specific';
+// import './tests-runtimeToBatchEventsDTO';
+// import './tests-apiClient';
+// import './tests-mparticle-instance-manager';
+// import './tests-queue-public-methods';
+// import './tests-validators';
+// import './tests-utils';
+// import './tests-store';
+// import './tests-config-api-client';
+// import './tests-vault';
