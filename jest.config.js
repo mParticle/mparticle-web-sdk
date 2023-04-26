bfbcb3da2bf137668addfcdac4f546f589bb7313
Alexander Sapountzis <alexs@mparticle.com>
2023-04-26 15:15:43 -0400

    Update Jest Config

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,5 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
 
-    // Ignore all legecy tests for now until all tests are migrated
-    // to support JEST
-    modulePathIgnorePatterns: ['./test/*'],
-
     setupFilesAfterEnv: ['jest-expect-message'],
 };
