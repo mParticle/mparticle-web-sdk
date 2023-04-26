e86683d638a88f68284f6df3f98ff5492845f2d0
Alex S <49695018+alexs-mparticle@users.noreply.github.com>
2023-04-26 15:08:41 -0400

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,9 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
 
-    // modulePaths: ['./src/foo.test.ts', './test/src/tests-vault.ts'],
-
     // Ignore all legecy tests for now until all tests are migrated
     // to support JEST
-    // modulePathIgnorePatterns: ['./test/*'],
+    modulePathIgnorePatterns: ['./test/*'],
 
     setupFilesAfterEnv: ['jest-expect-message'],
 };
