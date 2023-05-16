36483db77fb7e145d907e09cc4b9a69ecabc5fc4
Robert Ing <rmi22186@gmail.com>
2023-05-16 10:42:47 -0400

    Prettierify, update filterDictionary

--- src/sideloadedKit.ts
+++ src/sideloadedKit.ts
@@ -1,10 +1,12 @@
 import KitFilterHelper from './kitFilterHelper';
-import { IKitFilterSettings } from './configAPIClient';
+import {
+    IFilteringConsentRuleValues,
+    IFilteringEventAttributeValue,
+    IFilteringUserAttributeValue,
+    IKitFilterSettings,
+} from './configAPIClient';
 import { UnregisteredKit } from './forwarders.interfaces';
 import { EventTypeEnum, IdentityType } from './types.interfaces';
-
-/**
- */
 export class MPSideloadedKit {
     public kitInstance: UnregisteredKit;
     public filterDictionary: IKitFilterSettings = {
@@ -19,47 +21,80 @@ export class MPSideloadedKit {
         consentRegulationPurposeFilters: [],
         messageTypeFilters: [],
         messageTypeStateFilters: [],
-        filteringEventAttributeValue: {},
-        filteringUserAttributeValue: {},
-        filteringConsentRuleValues: {},
-    } as IKitFilterSettings;
+
+        // The below filtering members are optional, but we instantiate them
+        // to simplify public method assignment
+        filteringEventAttributeValue: {} as IFilteringEventAttributeValue,
+        filteringUserAttributeValue: {} as IFilteringUserAttributeValue,
+        filteringConsentRuleValues: {} as IFilteringConsentRuleValues,
+    };
 
     constructor(unregisteredKitInstance: UnregisteredKit) {
         this.kitInstance = unregisteredKitInstance;
     }
 
-    public addEventTypeFilter(eventType: EventTypeEnum) {
-        const hashedEventType  = KitFilterHelper.hashEventType(eventType);
+    public addEventTypeFilter(eventType: EventTypeEnum): void {
+        const hashedEventType = KitFilterHelper.hashEventType(eventType);
         this.filterDictionary.eventTypeFilters.push(hashedEventType);
     }
 
-    public addEventNameFilter(eventType: EventTypeEnum, eventName: string) {
-        const hashedEventName  = KitFilterHelper.hashEventName(eventName, eventType);
+    public addEventNameFilter(
+        eventType: EventTypeEnum,
+        eventName: string
+    ): void {
+        const hashedEventName = KitFilterHelper.hashEventName(
+            eventName,
+            eventType
+        );
         this.filterDictionary.eventNameFilters.push(hashedEventName);
     }
-    
-    public addEventAttributeFilter(eventType: EventTypeEnum, eventName: string, customAttributeKey: string) {
-        const hashedEventAttribute  = KitFilterHelper.hashEventAttributeKey(eventType, eventName, customAttributeKey);
+
+    public addEventAttributeFilter(
+        eventType: EventTypeEnum,
+        eventName: string,
+        customAttributeKey: string
+    ): void {
+        const hashedEventAttribute = KitFilterHelper.hashEventAttributeKey(
+            eventType,
+            eventName,
+            customAttributeKey
+        );
         this.filterDictionary.attributeFilters.push(hashedEventAttribute);
     }
 
-    public addScreenNameFilter(screenName: string) {
-        const hashedScreenName = KitFilterHelper.hashEventName(screenName, EventTypeEnum.Unknown);
+    public addScreenNameFilter(screenName: string): void {
+        const hashedScreenName = KitFilterHelper.hashEventName(
+            screenName,
+            EventTypeEnum.Unknown
+        );
         this.filterDictionary.screenNameFilters.push(hashedScreenName);
     }
 
-    public addScreenAttributeFilter(screenName: string, screenAttribute: string) {
-        const hashedScreenAttribute = KitFilterHelper.hashEventAttributeKey(EventTypeEnum.Unknown, screenName, screenAttribute);
-        this.filterDictionary.screenAttributeFilters.push(hashedScreenAttribute);
+    public addScreenAttributeFilter(
+        screenName: string,
+        screenAttribute: string
+    ): void {
+        const hashedScreenAttribute = KitFilterHelper.hashEventAttributeKey(
+            EventTypeEnum.Unknown,
+            screenName,
+            screenAttribute
+        );
+        this.filterDictionary.screenAttributeFilters.push(
+            hashedScreenAttribute
+        );
     }
 
-    public addUserIdentityFilter(userIdentity: IdentityType){
-        const hashedIdentityType = KitFilterHelper.hashUserIdentity(userIdentity);
+    public addUserIdentityFilter(userIdentity: IdentityType): void {
+        const hashedIdentityType = KitFilterHelper.hashUserIdentity(
+            userIdentity
+        );
         this.filterDictionary.userIdentityFilters.push(hashedIdentityType);
     }
 
-    public addUserAttributeFilter(userAttributeKey: string) {
-        const hashedUserAttributeKey = KitFilterHelper.hashUserAttribute(userAttributeKey);
+    public addUserAttributeFilter(userAttributeKey: string): void {
+        const hashedUserAttributeKey = KitFilterHelper.hashUserAttribute(
+            userAttributeKey
+        );
         this.filterDictionary.userAttributeFilters.push(hashedUserAttributeKey);
     }
 }
