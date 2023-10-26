69533b6a75fcc7715935ecf0a40c2e3e73673e82
Robert Ing <rmi22186@gmail.com>
2023-10-26 15:27:42 -0400

    test: Transpile tests to run older browsers in browserstack (#795)

---
+++ test/cross-browser-testing/rollup.CBT.config.js
@@ -0,0 +1,63 @@
+import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import babelSettings from './babel.CBT.config.js'
+const extensions = ['.js', '.ts'];
+import typescript from '@rollup/plugin-typescript';
+import json from '@rollup/plugin-json';
+
+export default [
+    {
+        input: 'test/src/tests-main.js',
+        output: {
+            file: 'test/cross-browser-testing/CBT-tests.js',
+            format: 'umd',
+            name: 'mParticleTests',
+        },
+        plugins: [
+            resolve({
+                preferBuiltins: true,
+            }),
+            commonjs({
+                include: 'node_modules/**',
+            }),
+            typescript({
+                tsconfig: './tsconfig.json',
+                sourceMap: false,
+            }),
+            babel({
+                presets: ['@babel/preset-env'],
+                plugins: ['@babel/plugin-transform-runtime'],
+                extensions,
+                // ...babelSettings,
+                // exclude: ['node_modules/**/*'],
+                babelHelpers: 'runtime',
+            }),
+            json()
+        ],
+    },
+    {
+        input: 'test/cross-browser-testing/CBT-tests.js',
+        output: {
+            file: 'test/cross-browser-testing/CBT-tests-es5.js',
+            format: 'umd',
+            name: 'mParticleTests',
+        },
+        plugins: [
+            resolve({
+                preferBuiltins: true
+            }),
+            commonjs({
+                include: 'node_modules/**',
+            }),
+            babel({
+                presets: ['@babel/preset-env'],
+                plugins: ['@babel/plugin-transform-runtime'],
+                ...babelSettings,
+                exclude: ['node_modules/**/*'],
+                babelHelpers: 'runtime',
+            }),
+            json()
+        ],
+    },
+]
--- test/cross-browser-testing/test-cross-browser.js
+++
@@ -1,8 +0,0 @@
-describe('test browserstack integration', function() {
-    it('should pass', function() {
-        true.should.equal(true);
-    });
-    it('should not pass on older browsers', function() {
-        'foo-bar'.replaceAll('o', 0).should.equal('f00-bar');
-    });
-});
