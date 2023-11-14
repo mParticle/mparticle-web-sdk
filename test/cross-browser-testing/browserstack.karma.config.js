d6025007d6992fa8ea7b0a4f0b01f1a7a5e0060c
Robert Ing <rmi22186@gmail.com>
2023-11-14 17:24:29 -0500

    ci: Add legacy browsers for testing

--- test/cross-browser-testing/browserstack.karma.config.js
+++ test/cross-browser-testing/browserstack.karma.config.js
@@ -25,22 +25,53 @@ if (DEBUG === 'true') {
 
 const customLaunchers = {
     // Full list of supported browsers - https://www.browserstack.com/list-of-browsers-and-platforms/live
-    // Fails a few tests related to setTimeout
-    bs_chrome_mac_48: {
+    // https://www.w3schools.com/js/js_versions.asp shows a list of browsers that support ES6.  
+    // The below list is primarily the version just before that, or if that version was not available on Browserstack to test, the next version was
+    // All versions below, including earlier versions of each browser, have a combined ~0.37% market share according to
+    // www.browserlist.dev.  Query for "opera < 38, safari < 12, chrome < 51, firefox <52, edge < 15"
+    bs_chrome_mac_50: {
       base: 'BrowserStack',
       browser: 'chrome',
-      browser_version: '48.0',
+      browser_version: '50.0',
+      os: 'OS X',
+      os_version: 'Mojave'
+    },
+    bs_firefox_mac_51: {
+      base: 'BrowserStack',
+      browser: 'firefox',
+      browser_version: '51.0',
+      os: 'OS X',
+      os_version: 'Mojave'
+    },
+    bs_edge_windows_15: {
+      base: 'BrowserStack',
+      browser: 'edge',
+      browser_version: '15.0',
+      os: 'Windows',
+      os_version: '10'
+    },
+    bs_safari_mac_11: {
+      base: 'BrowserStack',
+      browser: 'safari',
+      browser_version: '11.1',
+      os: 'OS X',
+      os_version: 'High Sierra'
+    },
+    bs_opera_mac_37: {
+      base: 'BrowserStack',
+      browser: 'opera',
+      browser_version: '37.0',
       os: 'OS X',
       os_version: 'Mojave'
     },
     // Oldest Chrome version to pass all tests
-    bs_chrome_mac_49: {
-      base: 'BrowserStack',
-      browser: 'chrome',
-      browser_version: '49.0',
-      os: 'OS X',
-      os_version: 'Mojave'
-    },
+    // bs_chrome_mac_49: {
+    //   base: 'BrowserStack',
+    //   browser: 'chrome',
+    //   browser_version: '49.0',
+    //   os: 'OS X',
+    //   os_version: 'Mojave'
+    // },
 }
 
 module.exports = function(config) {
