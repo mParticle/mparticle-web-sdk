85a01f0cdadbd50fd2306d7e18977931a9211c3f
Robert Ing <ring@mparticle.com>
2024-03-06 10:57:30 -0500

    remove commented code

--- src/audienceManager.ts
+++ src/audienceManager.ts
@@ -11,37 +11,6 @@ export interface IAudienceMemberships {
     audience_memberships: Audience[];
 }
 
-// export interface IAudienceMemberships extends IAudienceServerResponse {}
-
-// export interface IAudienceServerResponse {
-//     dt: AudienceResponseMessage;
-//     id: string;
-//     ct: number;
-//     m: IMinifiedAudienceMembership[];
-// }
-// export interface IMinifiedAudienceMembership {
-//     id: number; // AudienceId
-//     n: string; // External Name
-//     c: AudienceListMembershipUpdateType[]; // AudienceMembershipChanges
-//     s: string[]; // ModuleEndpointExternalReferenceKeys
-// }
-
-// export enum AudienceMembershipChangeAction {
-//     Unknown = 'unknown',
-//     Add = 'add',
-//     Drop = 'drop',
-// }
-
-// export interface AudienceListMembershipUpdateType {
-//     ct: number;
-//     a: AudienceMembershipChangeAction;
-// }
-
-// export interface IMPParsedAudienceMemberships {
-//     currentAudiences: Audience[];
-//     pastAudiences: Audience[];
-// }
-
 export default class AudienceManager {
     public url: string = '';
     public userAudienceAPI: AsyncUploader;
@@ -116,28 +85,3 @@ export default class AudienceManager {
         }
     }
 }
-
-// export const parseUserAudiences = (audienceServerResponse: IAudienceServerResponse): IAudienceMemberships => {
-//     return audienceServerResponse;
-//     // const currentAudiences: Audience[] = [];
-//     // const pastAudiences: Audience[] = [];
-//     // audienceServerResponse.audience_memberships.forEach(audience: Audience) => {
-//         // if (membership.c[0].a === AudienceMembershipChangeAction.Add) {
-//         //     currentAudiences.push(new Audience(
-//         //         membership.id,
-//         //         membership.n
-//         //     ));
-//         // };
-
-//         // if (membership.c[0].a === AudienceMembershipChangeAction.Drop) {
-//         //     pastAudiences.push(new Audience(
-//         //         membership.id,
-//         //         membership.n
-//         //     ));
-//         // };
-//     // });
-
-//     // return {
-//     //     currentAudiences, pastAudiences
-//     // }
-// }
