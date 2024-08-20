2103ad8d9823362a95f6e7029e3fd5ec55a966d8
Robert Ing <ring@mparticle.com>
2024-08-20 13:11:06 -0400

    undo fetch removal

--- test/src/tests-config-api-client.ts
+++ test/src/tests-config-api-client.ts
@@ -215,8 +215,6 @@ describe('ConfigAPIClient', () => {
                         config,
                         window.mParticle.getInstance()
                     );
-                    const previousFetch = window.fetch;
-                    delete window.fetch;
 
                     const response =
                         await configAPIClient.getSDKConfiguration();
@@ -230,8 +228,6 @@ describe('ConfigAPIClient', () => {
                     expect(response).to.be.ok;
                     expect(response.appName).to.equal('Test App');
                     expect(response.kitConfigs).to.deep.equal(kitConfigs);
-
-                    window.fetch = previousFetch;
                 });
 
                 it('should return inital config fetch fails', async () => {
