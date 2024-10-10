f28aaa3ffdc4032d6c9024a0f3ac07f4a7816f45
Robert Ing <ring@mparticle.com>
2024-10-10 17:10:26 -0400

    fix requirejs tests

--- test/integrations/requirejs/test-requirejs.js
+++ test/integrations/requirejs/test-requirejs.js
@@ -1,16 +1,43 @@
 import fetchMock from 'fetch-mock/esm/client';
-import sinon from 'sinon';
+
+const waitForCondition = function async(
+        conditionFn,
+        timeout = 200,
+        interval = 10
+    ) {
+        return new Promise((resolve, reject) => {
+            const startTime = Date.now();
+
+            (function poll() {
+                if (conditionFn()) {
+                    return resolve(undefined);
+                } else if (Date.now() - startTime > timeout) {
+                    return reject(new Error('Timeout waiting for condition'));
+                } else {
+                    setTimeout(poll, interval);
+                }
+            })();
+        });
+    },
+    fetchMockSuccess = function (url, body) {
+        fetchMock.post(
+            url,
+            {
+                status: 200,
+                body: JSON.stringify(body),
+            },
+            { overwriteRoutes: true }
+        );
+    },
+    hasIdentifyReturned = () => {
+        return window.mParticle.Identity.getCurrentUser()?.getMPID() === 'testMPID';
+};
 
 describe('Require.JS Pages', function() {
     it('loads mParticle properly', function() {
-        var mockServer = sinon.createFakeServer();
-        mockServer.respondImmediately = true;
-
-        mockServer.respondWith('https://identity.mparticle.com/v1/identify', [
-            200,
-            {},
-            JSON.stringify({ mpid: 'testMPID', is_logged_in: false }),
-        ]);
+        fetchMockSuccess('https://identity.mparticle.com/v1/identify', {
+            mpid: 'testMPID', is_logged_in: false
+        });
 
         fetchMock.post('https://jssdks.mparticle.com/v3/JS/test_key/events', 200);
 
@@ -26,7 +53,7 @@ describe('Require.JS Pages', function() {
         };
 
         mParticle.init('test_key', window.mParticle.config);
-
+        waitForCondition(hasIdentifyReturned).then(() => {
         fetchMock.resetHistory();
 
         mParticle.logEvent('Test Event1');
@@ -35,5 +62,6 @@ describe('Require.JS Pages', function() {
         const testEventName = JSON.parse(testEvent[1].body).events[0].data.event_name;
 
         testEventName.should.equal('Test Event1');
+        })
     });
 });
