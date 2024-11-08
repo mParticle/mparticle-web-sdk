c3642b4f0e8842a28b860274dbddd477990ad8e7
Alexander Sapountzis <alexs@mparticle.com>
2024-11-08 09:54:11 -0500

    Address PR Comments

--- src/identityApiClient.interfaces.ts
+++ src/identityApiClient.interfaces.ts
@@ -22,7 +22,7 @@ export interface IIdentityApiClient {
         method: IdentityAPIMethod,
         callback: IdentityCallback,
         originalIdentityApiData: IdentityApiData,
-        parseIdentityResponse: IIdentity['parseIdentityResponse'],
+        parseIdentityResponse: Pick<IIdentity, 'parseIdentityResponse'>,
         mpid: MPID,
         knownIdentities: UserIdentities
     ) => Promise<void>;
