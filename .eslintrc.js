1b602f33a4f6f70f477dd657826c4552daf163f4
Alexander Sapountzis <alexs@mparticle.com>
2025-02-10 14:37:04 -0500

    build: Update Eslint Process

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+    env: {
+        browser: true,
+        node: true,
+    },
+    parser: '@babel/eslint-parser',
+    parserOptions: {
+        ecmaVersion: 6,
+        sourceType: 'module',
+        ecmaFeatures: {
+            modules: true,
+        },
+    },
+    globals: {
+        mParticle: true,
+        describe: true,
+        assert: true,
+        Should: true,
+        MockHttpServer: true,
+        it: true,
+        sinon: true,
+        const: true,
+        before: true,
+        beforeEach: true,
+        afterEach: true,
+        after: true,
+        Promise: true,
+    },
+    extends: [
+        // Recommended rules
+        'eslint:recommended',
+
+        // Turn off conflicting rules
+        'prettier',
+    ],
+    rules: {
+        // 'prettier/prettier': 'error',
+        'prefer-const': [
+            'warn',
+            {
+                destructuring: 'any',
+                ignoreReadBeforeAssign: false,
+            },
+        ],
+        'no-prototype-builtins': 'off',
+        'no-empty': 'warn',
+        'no-useless-escape': 'off',
+    },
+};
