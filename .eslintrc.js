0074acbf184808cd002a70814ed5c17777c717e8
Alexander Sapountzis <alexs@mparticle.com>
2025-02-11 16:15:59 -0500

    Update rules to lint typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,6 @@ module.exports = {
         node: true,
     },
     ignorePatterns: ['node_modules', 'dist'],
-    parser: '@babel/eslint-parser',
     parserOptions: {
         ecmaVersion: 6,
         sourceType: 'module',
@@ -35,12 +34,45 @@ module.exports = {
         // Turn off conflicting rules
         'prettier',
     ],
-    plugins: ['prettier'],
+    plugins: ['@typescript-eslint', 'prettier'],
+    overrides: [
+        {
+            files: ['*.ts'],
+            parser: '@typescript-eslint/parser',
+            extends: ['plugin:@typescript-eslint/recommended'],
+            rules: {
+                '@typescript-eslint/explicit-function-return-type': 'off',
+                '@typescript-eslint/ban-ts-ignore': 'off',
+                '@typescript-eslint/ban-ts-comment': 'off',
+                '@typescript-eslint/no-empty-interface': 'off',
+                '@typescript-eslint/no-explicit-any': 'off',
+                '@typescript-eslint/no-non-null-assertion': 'off',
+                '@typescript-eslint/no-this-alias': 'off',
+                'require-await': 'off',
+                '@typescript-eslint/no-empty-function': 'off',
+            },
+        },
+        {
+            files: ['*.js'],
+            parser: '@babel/eslint-parser',
+
+            rules: {
+                'prefer-const': [
+                    'error',
+                    {
+                        destructuring: 'any',
+                        ignoreReadBeforeAssign: false,
+                    },
+                ],
+            },
+        },
+    ],
     rules: {
         'prettier/prettier': 'error',
         'no-prototype-builtins': 'off',
         'no-empty': 'warn',
         'no-unused-vars': 'warn',
         'no-useless-escape': 'off',
+        'no-var': 'warn',
     },
 };
