34feff4e968c125f5a11db173b29862f13fbff4c
Alexander Sapountzis <alexs@mparticle.com>
2025-03-18 16:47:33 -0400

    Save Point

--- src/configAPIClient.ts
+++ src/configAPIClient.ts
@@ -5,7 +5,6 @@ import {
     SDKEventCustomFlags,
     SDKInitConfig,
 } from './sdkRuntimeModels';
-import { Dictionary } from './utils';
 import {
     AsyncUploader,
     IFetchPayload,
@@ -14,7 +13,11 @@ import {
 } from './uploaders';
 import { IPixelConfiguration } from './cookieSyncManager';
 import { IMParticleWebSDKInstance } from './mp-instance';
-import { ForwarderSettings, UserAttributeFilters, UserIdentityFilters } from './forwarders.interfaces';
+import {
+    ForwarderSettings,
+    UserAttributeFilters,
+    UserIdentityFilters,
+} from './forwarders.interfaces';
 
 export interface IKitConfigs extends IKitFilterSettings {
     name: string;
