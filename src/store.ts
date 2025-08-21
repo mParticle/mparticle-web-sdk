2e731a046ddacbc83e648bebf29ed4042d1ea90a
Jaissica Hora <jaissicahora@Jaissicas-MacBook-Pro.local>
2025-08-21 13:50:53 -0400

    chore: updated flag casing

--- src/store.ts
+++ src/store.ts
@@ -737,7 +737,7 @@ export function processFlags(config: SDKInitConfig): IFeatureFlags {
     flags[CacheIdentity] = config.flags[CacheIdentity] === 'True';
     flags[AudienceAPI] = config.flags[AudienceAPI] === 'True';
     flags[CaptureIntegrationSpecificIds] = config.flags[CaptureIntegrationSpecificIds] === 'True';
-    flags[CaptureIntegrationSpecificIdsV2] = (config.flags[CaptureIntegrationSpecificIdsV2].toLowerCase() || 'none');
+    flags[CaptureIntegrationSpecificIdsV2] = (config.flags[CaptureIntegrationSpecificIdsV2] || 'none');
     flags[AstBackgroundEvents] = config.flags[AstBackgroundEvents] === 'True';
     return flags;
 }
