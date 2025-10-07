68bc1d1546c7afe7f212731be314d791a95cff3e
Jaissica <jaissica.hora@rokt.com>
2025-10-07 16:00:59 -0400

    chore: remove .only from self-hosting-specific test

--- test/src/tests-self-hosting-specific.js
+++ test/src/tests-self-hosting-specific.js
@@ -12,7 +12,7 @@ const {
 } = Utils;
 
 // Calls to /config are specific to only the self hosting environment
-describe.only('/config self-hosting integration tests', function() {
+describe('/config self-hosting integration tests', function() {
     beforeEach(function() {
         fetchMock.post(urls.events, 200);
     });
