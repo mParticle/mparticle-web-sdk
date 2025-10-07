3eae2b20bfeb040e9e9749b80906f895d11fd965
Jaissica <jaissica.hora@rokt.com>
2025-10-07 15:58:27 -0400

    chore: remove .only from serverModel test

--- test/src/tests-serverModel.ts
+++ test/src/tests-serverModel.ts
@@ -20,7 +20,7 @@ let initialEvent = {};
 const mParticle = window.mParticle;
 const ServerModel = mParticle.getInstance()._ServerModel;
 
-describe.only('ServerModel', () => {
+describe('ServerModel', () => {
     beforeEach(() => {
         initialEvent = {
             messageType: Types.MessageType.PageEvent,
