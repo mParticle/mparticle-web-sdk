6bbe211764638b0a5235d744594019faf492b4ee
Khushi Patel <146786769+khushi1033@users.noreply.github.com>
2026-02-03 11:11:19 -0500

    Update src/roktManager.ts
    Co-authored-by: Robert Ing <rmi22186@gmail.com>

--- src/roktManager.ts
+++ src/roktManager.ts
@@ -164,7 +164,7 @@ export default class RoktManager {
     }
 
     public get isInitialized(): boolean {
-        return this.isInitialized;
+        return this.initialized;
     }
 
     public attachKit(kit: IRoktKit): void {
