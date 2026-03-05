820fa4226a7bbd6bf9fc91ae32054125cb7a6761
Nickolas Dimitrakas <nickolas.dimitrakas@rokt.com>
2026-03-05 16:15:13 -0500

    feat: migrate amplitude (#1173)
    * feat: migrate amplitude
    
    * formatting
    
    * added job to build and run kit tests
    
    * align pull request workflow
    
    * slight pull request needs adjustments
    
    * add kits to release process
    
    * add homepage url
    
    * Revert "slight pull request needs adjustments"
    
    This reverts commit aaf841621902a4befefd04b203188b53a2dde1f9.
    
    * reuse mockhttprequest from root
    
    * address @alexs-mparticle comments
    
    * add mparticle core dependency
    
    * only use root .gitignore
    
    * use root prettier
    
    * inherit from root eslintrc
    
    * cleanup kit directory readme
    
    * restore original readme
    
    * peerDependency already states mP Core
    
    * cleanup matrix
    
    * update to kit track package name
    
    * adjust semantic release to update kit versions in lock step
    
    * pass proper commit sha
    
    * unique artifact names and align jest matrix
    
    * add cleanup step for deleting release branch
    
    * publish kits upon success of core and fix issues in implementation
    
    * Configure dependabot for amplitude kit track
    
    * Update kits/README.md
    
    Co-authored-by: Robert Ing <rmi22186@gmail.com>
    
    * run format to fix ci
    
    * add canonical repo chart in readme
    
    * addressed @rmi22186 comments
    
    ---------
    
    Co-authored-by: Robert Ing <rmi22186@gmail.com>

---
+++ kits/amplitude/amplitude-8/rollup.config.js
@@ -0,0 +1,52 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
+export default [
+    {
+        input: 'src/Amplitude.js',
+        output: {
+            file: 'dist/Amplitude.iife.js',
+            format: 'iife',
+            exports: 'named',
+            name: 'mpAmplitudeKit',
+            strict: false,
+        },
+        plugins: [
+            resolve({
+                browser: true,
+            }),
+            commonjs(),
+        ],
+    },
+    {
+        input: 'src/Amplitude.js',
+        output: {
+            file: 'dist/Amplitude.common.js',
+            format: 'cjs',
+            exports: 'named',
+            name: 'mpAmplitudeKit',
+            strict: false,
+        },
+        plugins: [
+            resolve({
+                browser: true,
+            }),
+            commonjs(),
+        ],
+    },
+    {
+        input: 'src/Amplitude.js',
+        output: {
+            file: 'dist/Amplitude.esm.js',
+            format: 'esm',
+            exports: 'named',
+            strict: false,
+        },
+        plugins: [
+            resolve({
+                browser: true,
+            }),
+            commonjs(),
+        ],
+    },
+];
