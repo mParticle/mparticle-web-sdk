d3eefb3685b020ae1c4e08ee2381462b91c366c6
Nickolas Dimitrakas <nickolas.dimitrakas@rokt.com>
2026-03-05 18:27:26 -0500

    fix(ci): apply prettier formatting to bingads kit

--- kits/bingads/rollup.config.js
+++ kits/bingads/rollup.config.js
@@ -9,14 +9,14 @@ export default [
             format: 'iife',
             exports: 'named',
             name: 'mpBingAdsKit',
-            strict: false
+            strict: false,
         },
         plugins: [
             resolve({
-                browser: true
+                browser: true,
             }),
-            commonjs()
-        ]
+            commonjs(),
+        ],
     },
     {
         input: 'src/BingAdsEventForwarder',
@@ -25,13 +25,13 @@ export default [
             format: 'cjs',
             exports: 'named',
             name: 'mpBingAdsKit',
-            strict: false
+            strict: false,
         },
         plugins: [
             resolve({
-                browser: true
+                browser: true,
             }),
-            commonjs()
-        ]
-    }
-]
+            commonjs(),
+        ],
+    },
+];
