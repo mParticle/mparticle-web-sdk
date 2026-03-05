f2207ed8625f534bf68eb8df601ff9d9e64205ff
Nickolas Dimitrakas <nickolas.dimitrakas@rokt.com>
2026-03-05 18:27:26 -0500

    fix(ci): apply prettier formatting to simplereach kit

--- kits/simplereach/rollup.config.js
+++ kits/simplereach/rollup.config.js
@@ -9,14 +9,14 @@ export default [
             format: 'iife',
             exports: 'named',
             name: 'mpSimpleReachKit',
-            strict: false
+            strict: false,
         },
         plugins: [
             resolve({
-                browser: true
+                browser: true,
             }),
-            commonjs()
-        ]
+            commonjs(),
+        ],
     },
     {
         input: 'src/SimpleReach.js',
@@ -25,13 +25,13 @@ export default [
             format: 'cjs',
             exports: 'named',
             name: 'mpSimpleReachKit',
-            strict: false
+            strict: false,
         },
         plugins: [
             resolve({
-                browser: true
+                browser: true,
             }),
-            commonjs()
-        ]
-    }
-]
+            commonjs(),
+        ],
+    },
+];
