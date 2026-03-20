e573c1ff57418944451360dbdfe5b253f60b78f5
Alexander Sapountzis <alexs@mparticle.com>
2026-03-20 17:14:51 -0400

    chore: remove dist/ from tracking (already in gitignore)

--- dist/src/mockBatchCreator.d.ts
+++
@@ -1,6 +0,0 @@
-import { BaseEvent } from './sdkRuntimeModels';
-import * as EventsApi from '@mparticle/event-models';
-export default class _BatchValidator {
-    private getMPInstance;
-    returnBatch(event: BaseEvent): EventsApi.Batch;
-}
