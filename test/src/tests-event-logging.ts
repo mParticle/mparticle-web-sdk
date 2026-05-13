0c785965283c3881e4e7e5accf897d0a43c4201f
Jaissica <jaissica.hora@rokt.com>
2026-05-13 07:09:38 -0400

    fix SonarCloud issues

--- test/src/tests-event-logging.ts
+++ test/src/tests-event-logging.ts
@@ -31,7 +31,7 @@ declare global {
     }
 }
 
-const mParticle = window.mParticle as IMParticleInstanceManager;
+const mParticle = window.mParticle;
 
 const { findEventFromRequest, findBatch, getIdentityEvent, waitForCondition, fetchMockSuccess, hasIdentifyReturned } = Utils;
 
